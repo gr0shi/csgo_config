89c1ae1f1a91adc2a05af4d6adc7cf3f8797609b
rawreflect <maxjmagic@gmail.com>
2023-02-04 20:47:06 +0300

    add some stuff

--- copy-cfg-files-windows.cmd
+++ copy-cfg-files-windows.cmd
@@ -5,6 +5,7 @@ SET csgo_install_path=%PROGRAMFILES(x86)%\Steam\steamapps\common\Counter-Strike 
 
 XCOPY %~dp0cfg\* "%csgo_install_path%\csgo\cfg" /Y /F
 XCOPY %~dp0resource\* "%csgo_install_path%\csgo\resource" /Y /F
+XCOPY %~dp0scripts\* "%csgo_install_path%\csgo\scripts" /Y /F
 
 PAUSE
 EXIT /B 0
