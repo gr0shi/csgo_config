226e2181f48a3a68bca460bc83939580fe4035b1
rawreflect <maxjmagic@gmail.com>
2023-02-25 10:42:37 +0300

    some stuff

---
+++ forcs/other/cs.bat
@@ -0,0 +1,10 @@
+@echo off
+ 
+start "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\" "steam://rungameid/730"
+ 
+start "C:\Users\maxjm\needed for cs\" "C:\Users\maxjm\needed for cs\bhop.ahk"
+
+start "C:\Users\maxjm\needed for cs\" "C:\Users\maxjm\needed for cs\vibranceGUI"
+ 
+ 
+exit
